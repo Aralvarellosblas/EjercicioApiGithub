0d898c8b671d65e108fe05c7a656a6f31bb3fa1a
arturo <arturo@ArturoMSI>
2018-04-17 18:54:18 +0200

    Menú y Javadoc

--- src/main/java/com/mycompany/ejemplomaven/EjemploMaven.java
+++ src/main/java/com/mycompany/ejemplomaven/EjemploMaven.java
@@ -1,6 +1,10 @@
 package com.mycompany.ejemplomaven;
 
+import javax.swing.JOptionPane;
+
 /**
+ * Clase principal contenedora del metodo main del programa con el menú para la
+ * seleccion de los distintos metodos
  *
  * @author Arturo
  */
@@ -10,7 +14,28 @@ public class EjemploMaven{
      * @param args the command line arguments
      */
     public static void main(String[] args){
-        
+        String[] menu={"Crear repositorio", "Clonar", "Commit", "Inicializar un repositorio", "Push", "Salir"};
+        int opcion=JOptionPane.showOptionDialog(null, "Seleccione una opción", "GitHub", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, menu, null);
+
+        switch(opcion){
+            case 0:
+                Metodos.crearRepositorio();
+                break;
+            case 1:
+                Metodos.clonarRepositorio();
+                break;
+            case 2:
+                Metodos.commit();
+                break;
+            case 3:
+                Metodos.inicializarRepositorio();
+                break;
+            case 4:
+                Metodos.push();
+                break;
+            case 5:
+                System.exit(0);
+        }
     }
 
 }
